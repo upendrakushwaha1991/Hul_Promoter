0655ed792bd3f0f4efee0f7deca8e3a6712977d4
upendrak <upendra.kushwaha@in.cpm-int.com>
2017-12-14 18:48:20 +0530

    upendra_14dec_mainmenuactivity

---
+++ intelRE/src/main/java/intelre/cpm/com/intelre/dailyentry/ServiceActivity.java
@@ -0,0 +1,73 @@
+package intelre.cpm.com.intelre.dailyentry;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import intelre.cpm.com.intelre.R;
+import intelre.cpm.com.intelre.constant.CommonString;
+
+public class ServiceActivity extends AppCompatActivity {
+
+    private SharedPreferences preferences;
+    String visitdate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //preference data
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        visitdate = preferences.getString(CommonString.KEY_DATE,"");
+        getSupportActionBar().setTitle("BackUp - "+visitdate);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+            overridePendingTransition(R.anim.activity_back_in, R.anim.activity_back_out);
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.activity_back_in, R.anim.activity_back_out);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
